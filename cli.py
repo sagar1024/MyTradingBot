6cd81e89255da6e0cd879fb8cb6e48b30b6ef65a
sagar <sagargurung272@gmail.com>
2025-07-10 00:10:17 +0530

    Cli done

--- cli.py
+++ cli.py
@@ -1,36 +1,42 @@
-from bot.core import MyTradingBot
+from bot.futures_client import BinanceFuturesREST
 
 def main():
-    bot = MyTradingBot()
+    bot = BinanceFuturesREST()
 
-    print("🚀 Welcome to MyTradingBot (CLI Version) 🚀\n")
+    print("Welcome to MyTradingBot (CLI Version)\n")
 
-    symbol = input("Enter trading pair (e.g., BTCUSDT): ").strip().upper()
-    side = input("Enter side (BUY/SELL): ").strip().upper()
-    order_type = input("Enter order type (MARKET / LIMIT / STOP_LIMIT): ").strip().upper()
-    quantity = float(input("Enter quantity: ").strip())
+    try:
+        symbol = input("Enter trading pair (e.g., BTCUSDT): ").strip().upper()
+        side = input("Enter side (BUY/SELL): ").strip().upper()
+        order_type = input("Enter order type (MARKET / LIMIT / STOP_LIMIT): ").strip().upper()
+        quantity = float(input("Enter quantity: ").strip())
 
-    price = None
-    stop_price = None
+        price = None
+        stop_price = None
 
-    if order_type in ["LIMIT", "STOP_LIMIT"]:
-        price = float(input("Enter limit price: ").strip())
-    if order_type == "STOP_LIMIT":
-        stop_price = float(input("Enter stop price: ").strip())
+        if order_type in ["LIMIT", "STOP_LIMIT"]:
+            price = float(input("Enter limit price: ").strip())
+        if order_type == "STOP_LIMIT":
+            stop_price = float(input("Enter stop price: ").strip())
 
-    print("\nPlacing order...")
-    
-    result = bot.place_order(
-        symbol=symbol,
-        side=side,
-        quantity=quantity,
-        order_type=order_type,
-        price=price,
-        stop_price=stop_price
-    )
+        print("\nPlacing order...")
 
-    print("\n📦 Order Result:")
-    print(result)
+        result = bot.place_order(
+            symbol=symbol,
+            side=side,
+            quantity=quantity,
+            order_type=order_type,
+            price=price,
+            stop_price=stop_price
+        )
+
+        print("\nOrder Result:")
+        for k, v in result.items():
+            print(f"{k}: {v}")
+
+    except Exception as e:
+        print(f"\nError: {e}")
 
 if __name__ == "__main__":
     main()
+    
